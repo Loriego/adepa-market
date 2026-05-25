116e9f35c62943dbca54e6b129503f42aead5d5b
Loriego <stallonerouffab@gmail.com>
2026-05-25 13:39:10 +0000

    Fix Tailwind PostCSS config

--- src/main.jsx
+++ src/main.jsx
@@ -1,32 +1,10 @@
-import { StrictMode } from "react";
-
-import { createRoot } from "react-dom/client";
-
-import { Toaster } from "react-hot-toast";
-
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 import "./index.css";
 
-import App from "./App.jsx";
-
-createRoot(
-  document.getElementById("root")
-).render(
-  <StrictMode>
-    <Toaster position="top-right" />
-
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>
+  </React.StrictMode>
 );
-
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker
-      .register("/sw.js")
-      .then(() =>
-        console.log("PWA Ready")
-      )
-      .catch((error) =>
-        console.log(error)
-      );
-  });
-}
