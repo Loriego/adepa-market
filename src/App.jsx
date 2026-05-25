2328ef798ca919348c516e87a71fcac21e252216
Loriego <stallonerouffab@gmail.com>
2026-05-25 12:29:50 +0000

    Fix admin and vendor routes

--- src/App.jsx
+++ src/App.jsx
@@ -32,6 +32,7 @@ import ManageProducts from "./admin/ManageProducts";
 import Orders from "./admin/Orders";
 import Vendors from "./admin/Vendors";
 import VendorPayouts from "./admin/VendorPayouts";
+import SendNotification from "./admin/SendNotification";
 
 // VENDOR
 import VendorLayout from "./vendor/VendorLayout";
@@ -49,6 +50,7 @@ export default function App() {
         <CartProvider>
           <BrowserRouter>
             <Routes>
+              {/* CUSTOMER ROUTES */}
               <Route path="/" element={<Home />} />
               <Route path="/shop" element={<Shop />} />
               <Route path="/cart" element={<Cart />} />
@@ -65,28 +67,51 @@ export default function App() {
               <Route path="/success" element={<Success />} />
               <Route path="/order-success" element={<OrderSuccess />} />
 
+              {/* SELLER SIGNUP / STORE */}
               <Route path="/vendor" element={<VendorSignup />} />
+              <Route path="/sell" element={<VendorSignup />} />
               <Route path="/vendor-signup" element={<VendorSignup />} />
               <Route path="/vendor-store" element={<VendorStore />} />
               <Route path="/vendor-store/:vendorId" element={<VendorStore />} />
 
+              {/* VENDOR DASHBOARD ROUTES */}
               <Route path="/vendor-dashboard" element={<VendorLayout />}>
                 <Route index element={<VendorDashboard />} />
-                <Route path="products" element={<VendorProducts />} />
+                <Route path="dashboard" element={<VendorDashboard />} />
                 <Route path="add-product" element={<VendorAddProduct />} />
+                <Route path="products" element={<VendorProducts />} />
                 <Route path="edit-product/:id" element={<VendorEditProduct />} />
                 <Route path="orders" element={<VendorOrders />} />
                 <Route path="earnings" element={<VendorEarnings />} />
               </Route>
 
+              {/* OLD VENDOR LINKS SUPPORT */}
+              <Route path="/vendor/dashboard" element={<VendorLayout />}>
+                <Route index element={<VendorDashboard />} />
+              </Route>
+              <Route path="/vendor/add-product" element={<VendorAddProduct />} />
+              <Route path="/vendor/products" element={<VendorProducts />} />
+              <Route path="/vendor/edit-product/:id" element={<VendorEditProduct />} />
+              <Route path="/vendor/orders" element={<VendorOrders />} />
+              <Route path="/vendor/earnings" element={<VendorEarnings />} />
+
+              {/* ADMIN ROUTES */}
               <Route path="/admin" element={<AdminDashboard />} />
+              <Route path="/admin/dashboard" element={<AdminDashboard />} />
               <Route path="/admin/add-product" element={<AddProduct />} />
               <Route path="/admin/edit-product/:id" element={<EditProduct />} />
+
+              {/* BOTH LINKS WORK NOW */}
+              <Route path="/admin/products" element={<ManageProducts />} />
               <Route path="/admin/manage-products" element={<ManageProducts />} />
+
               <Route path="/admin/orders" element={<Orders />} />
               <Route path="/admin/vendors" element={<Vendors />} />
               <Route path="/admin/vendor-payouts" element={<VendorPayouts />} />
+              <Route path="/admin/payouts" element={<VendorPayouts />} />
+              <Route path="/admin/send-notification" element={<SendNotification />} />
 
+              {/* 404 */}
               <Route path="*" element={<NotFound />} />
             </Routes>
           </BrowserRouter>
